dfe66dfef72bcaf2b6d948cdba073d1235597e96
terix2k11 <terixmail@googlemail.com>
2023-12-10 19:35:13 +0100

    basic game mechanics

--- voct1000react/src/App.js
+++ voct1000react/src/App.js
@@ -4,23 +4,18 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 
 
-function Card({cardData}) {
+function Card({cardData, showBack}) {
 
-    return (<div className="basic">{cardData.name} {cardData.shelf} </div>);
+    return (<div className="basic">{ showBack? cardData.name.split("").reverse().join("") : cardData.name} {cardData.shelf} </div>);
 }
 
 function App() {
 
     const [posts, setPosts] = useState({});
-
+    const [cards, setCards] = useState([]);
+    const [showBack, setShowBack] = useState(false);
+    const [index, setIndex] = useState(0);
     const [activeCard, setActiveCard] = useState({ name: "Void", shelf: -1});
-
-    const globalKeyPress = e => console.log(e);
-
-    useEffect( () => {
-        document.addEventListener('keyup', globalKeyPress)
-        // TODO how to remove eventlistener...?
-    },[]);
 
    useEffect(() => {
       fetch('http://localhost:8100')
@@ -28,6 +23,7 @@ function App() {
          .then((data) => {
             console.log("updated from fetch");
             setPosts(data);
+            setCards(data.data);
             setActiveCard( data.data[0]  )
          })
          .catch((err) => {
@@ -35,24 +31,56 @@ function App() {
          });
    }, []);
 
-   function promote(e)  {
-       setActiveCard(posts.data[1] );
+   function promote()  {
+       let newCard = {...activeCard}
+       newCard.shelf = newCard.shelf + 1;
+       setActiveCard( newCard )
    }
 
-    function demote(e)  {
-        setActiveCard(posts.data[0] );
+    function demote()  {
+        let newCard = {...activeCard}
+        newCard.shelf = newCard.shelf - 1;
+        setActiveCard( newCard )
+   }
+
+    function handleKey(e) {
+       // console.log(e);
+
+            if( e.key === "a") {
+                demote();
+            }
+            if( e.key === "d") {
+                promote();
+            }
+        if( e.key === "s") {
+            solve();
+        }
+        if( e.key === "d") {
+            skipCard();
+        }
+    }
+
+    function solve() {
+       setShowBack(!showBack);
+    }
+
+    function skipCard() {
+        setIndex(index + 1);
+        setActiveCard(cards[index+1]);
     }
 
   return (
-    <div className="App">
+    <div className="App"
+         onKeyUp={handleKey}
+    tabIndex={0}>
         <header className="App-header">
 
-            <Card cardData={ activeCard } />
+            <Card cardData={activeCard} showBack={showBack } />
 
             <button onClick={promote}>Promote</button>
             <button onClick={demote}>Demote</button>
-            <button>Skip</button>
-            <button>Solve</button>
+            <button onClick={solve}>Flip</button>
+            <button onClick={skipCard}>Skip</button>
 
         </header>
     </div>
