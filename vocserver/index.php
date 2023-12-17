b1dcf2994d899c183ba5f8ef026edc74b495809b
terix2k11 <terixmail@googlemail.com>
2023-12-17 11:45:51 +0100

    basic promot/demote for backend

--- vocserver/index.php
+++ vocserver/index.php
@@ -10,14 +10,15 @@ $username = 'devuser';
 $dbpassword = 'devpass';
 $dbname = 't1000_data_db';
 
-  	function nextCard() {
+  	function nextCard($idValue) {
 
         global $servername, $username, $dbpassword, $dbname;
         $content = array();
 
     	$mysqli = new mysqli($servername, $username, $dbpassword, $dbname);
 
-    	$stmt = $mysqli->prepare("SELECT ID, COLLECTION, BOX, CARD FROM TRAINING ORDER BY LAST_UPDATED ASC LIMIT 1");
+        $query = "SELECT ID, COLLECTION, BOX, CARD FROM TRAINING WHERE BOX = ".$idValue." ORDER BY LAST_UPDATED ASC LIMIT 1";
+    	$stmt = $mysqli->prepare($query);
     	$stmt->execute();
     	$stmt->bind_result($dId, $dCollection, $dBox, $dCard);
 
@@ -30,13 +31,51 @@ $dbname = 't1000_data_db';
 	    }
 
     	mysqli_close($mysqli);
-    	return $content;
+    	return $content[0];
   	}
 
-function save($data_back) {
+function getCardById($idValue) {
 
     global $servername, $username, $dbpassword, $dbname;
-    $content = array();
+
+    $mysqli = new mysqli($servername, $username, $dbpassword, $dbname);
+
+    $stmt = $mysqli->prepare("SELECT ID, FRONT, BACK FROM CARDS WHERE ID = " . $idValue);
+    $stmt->execute();
+    $stmt->bind_result($dId, $dFront, $dBack);
+    $stmt->fetch();
+
+    $item["id"] = $dId;
+    $item["front"] = $dFront;
+    $item["back"] = $dBack;
+
+    mysqli_close($mysqli);
+    return $item;
+}
+
+function getTrainingById($idValue) {
+
+    global $servername, $username, $dbpassword, $dbname;
+
+    $mysqli = new mysqli($servername, $username, $dbpassword, $dbname);
+
+    $stmt = $mysqli->prepare("SELECT ID, COLLECTION, CARD, BOX FROM TRAINING WHERE ID = " . $idValue);
+    $stmt->execute();
+    $stmt->bind_result($dId, $dCollection, $dCard, $dBox);
+    $stmt->fetch();
+
+    $item["id"] = $dId;
+    $item["collection"] = $dCollection;
+    $item["card"] = $dCard;
+    $item["box"] = $dBox;
+
+    mysqli_close($mysqli);
+    return $item;
+}
+
+function updateTraining($data_back) {
+
+    global $servername, $username, $dbpassword, $dbname;
 
     $mysqli = new mysqli($servername, $username, $dbpassword, $dbname);
     $query = "UPDATE TRAINING SET BOX = ". $data_back["box"] ." WHERE ID = ". $data_back["id"];
@@ -44,7 +83,7 @@ function save($data_back) {
     $stmt->execute();
 
     mysqli_close($mysqli);
-    return $content;
+    return;
 }
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
@@ -61,25 +100,47 @@ if($uriCommand == "hindi") {
     $cardCommand = $uriExploded[2];
 
     if($cardCommand == 'next') {
-        echo json_encode( nextCard() );
+        $idValue = intval($uriExploded[3]);
+        echo json_encode( nextCard($idValue) );
+        return;
+    }
+
+    if($cardCommand == 'get') {
+        $idValue = $uriExploded[3];
+        echo json_encode( getCardById($idValue) );
         return;
     }
 
     if($cardCommand == 'promote') {
-        $data_back = json_decode(file_get_contents('php://input'));
-        $box = $data_back->{"box"};
-        $id = $data_back->{"id"};
-        $collection = $data_back->{"collection"};
-        $card= $data_back->{"card"};
-        $new_data["id"] = $id;
-        $new_data["box"] = $box + 1;
-        $new_data["card"] = $card;
-        $new_data["collection"] = $collection;
-        echo json_encode( save($data_back) );
+        $idValue = $uriExploded[3];
+        $training = getTrainingById($idValue);
+        $training["box"] =  $training["box"] + 1;
+        updateTraining($training);
+        echo json_encode( true );
         return;
     }
 
     if($cardCommand == 'demote') {
+        $idValue = $uriExploded[3];
+        $training = getTrainingById($idValue);
+        $training["box"] =  $training["box"] - 1;
+        updateTraining($training);
+        echo json_encode( true );
+        return;
+    }
+
+    if($cardCommand == 'skip') {
+        $idValue = $uriExploded[3];
+        $training = getTrainingById($idValue);
+        $training["box"] =  $training["box"] - 1;
+        updateTraining($training);
+        $training["box"] =  $training["box"] + 1;
+        updateTraining($training);
+        echo json_encode( true );
+        return;
+    }
+
+    if($cardCommand == 'save') {
         $data_back = json_decode(file_get_contents('php://input'));
         $box = $data_back->{"box"};
         $id = $data_back->{"id"};
