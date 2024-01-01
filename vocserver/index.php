4b39b2c42c30f3313bd29006b78e6aaeea1e031b
terix2k12 <terixmail@googlemail.com>
2024-01-01 18:05:15 +0100

    offer backend login/logut stub

--- vocserver/index.php
+++ vocserver/index.php
@@ -23,6 +23,40 @@ $uriCommand = $uriExploded[1];
 
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type");
+
+if($uriCommand == "logout") {
+    header("HTTP/1.1 200 OK");
+    $success["info"] = "Logout successful!";
+    echo json_encode( $success );
+    return;
+}
+
+if($uriCommand == "login") {
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        header("HTTP/1.1 200 OK");
+        header("Access-Control-Allow-Methods: OPTIONS,POST");
+        $success["info"] = "Preflight information.";
+        echo json_encode( $success );
+        return;
+    }
+
+    $data_back = json_decode(file_get_contents('php://input'));
+    $user = $data_back->{"username"};
+    $pwd = $data_back->{"password"};
+
+    if($user == "foouser" && $pwd == "qwertz") {
+        header("HTTP/1.1 200 OK");
+        $success["success"] = "Login successful!";
+        echo json_encode( $success );
+        return;
+    } else {
+        header("HTTP/1.1 401 Access denied.");
+        $error["error"] = "Invalid credentials!";
+        echo json_encode( $error );
+        return;
+    }
+}
 
 if($uriCommand == "hindi") {
     $cardCommand = $uriExploded[2];
